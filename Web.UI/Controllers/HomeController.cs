c41f447e89b3e6fe6e49c662fc2adda7052f453a
emircokdogan <emircokdogan@gmail.com>
2016-08-17 00:35:14 +0300

    sayfalar eklendi

--- Web.UI/Controllers/HomeController.cs
+++ Web.UI/Controllers/HomeController.cs
@@ -1,11 +1,64 @@
 ﻿using System.Web.Mvc;
 
-namespace Web.UI.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace Web.UI.Controllers {
+    public class HomeController : Controller {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index() {
+            return View();
+        }
+
+        [Route("Kaliteli-Ceviri")]
+        public ActionResult QualityTranslation() {
+            return View();
+        }
+
+        [Route("Guvenlik-ve-Gizlilik")]
+        public ActionResult SecurityAndPrivacy() {
+            return View();
+        }
+
+        [Route("Banka-hesap-bilgileri")]
+        public ActionResult BillingInfo() {
+            return View();
+        }
+
+        [Route("Hizmet-Sozlesmesi")]
+        public ActionResult ServiceAggrement() {
+            return View();
+        }
+
+        [Route("Hakkimizda")]
+        public ActionResult About() {
+            return View();
+        }
+
+        [Route("Nasil-Calisiyor")]
+        public ActionResult HowItWorks() {
+            return View();
+        }
+
+        [Route("iletisim")]
+        public ActionResult Contact() {
+            return View();
+        }
+
+        [Route("is-firsatlari")]
+        public ActionResult Career() {
+            return View();
+        }
+
+        [Route("Basvur")]
+        public ActionResult ApplyForAJob(int jid) {
+            return View();
+        }
+
+        [Route("isDetaylari")]
+        public ActionResult JobDetail(int jid) {
+            return View();
+        }
+
+        [Route("tercume")]
+        public ActionResult Translation()
         {
             return View();
         }
