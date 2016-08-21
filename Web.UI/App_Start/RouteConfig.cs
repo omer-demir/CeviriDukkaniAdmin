4d3718ff4fd9bd1334b8b9a45169bb4e6d431a33
omer-demir <farukomer.demir@hepsiburada.com>
2016-08-21 19:21:32 +0300

    changes

--- Web.UI/App_Start/RouteConfig.cs
+++ Web.UI/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Web.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                defaults: new { controller = "Home", action = "TranslatorSignUp", id = UrlParameter.Optional },
                 namespaces: new[] { "Web.UI.Controllers" }
             );
         }
